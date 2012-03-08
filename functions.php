9b5badf515de188919c6b1786442e53294ad63ed
Tyler Cunningham <tyler@cyberchimps.com>
2012-03-08 13:36:31 -0800

    removed press trends, code and script cleanup

--- functions.php
+++ functions.php
@@ -268,49 +268,6 @@ require_once ( get_template_directory() . '/includes/classy-options-init.php' );
 require_once ( get_template_directory() . '/includes/options-functions.php' ); // Custom functions based on theme options.
 require_once ( get_template_directory() . '/includes/meta-box.php' ); // Meta options markup.
 
-// Presstrends
-function response_presstrends() {
-
-// Add your PressTrends and Theme API Keys
-$api_key = 'zwhgyc1lnt56hki8cpwobb47bblas4er226b';
-$auth = 'cfbu300xh4uq7o584ne59vwluhht0rign';
-
-// NO NEED TO EDIT BELOW
-$data = get_transient( 'presstrends_data' );
-if (!$data || $data == ''){
-$api_base = 'http://api.presstrends.io/index.php/api/sites/add/auth/';
-$url = $api_base . $auth . '/api/' . $api_key . '/';
-$data = array();
-$count_posts = wp_count_posts();
-$count_pages = wp_count_posts('page');
-$comments_count = wp_count_comments();
-$theme_data = get_theme_data(get_stylesheet_directory() . '/style.css');
-$plugin_count = count(get_option('active_plugins'));
-$all_plugins = get_plugins();
-foreach($all_plugins as $plugin_file => $plugin_data) {
-$plugin_name .= $plugin_data['Name'];
-$plugin_name .= '&';
-}
-$data['url'] = stripslashes(str_replace(array('http://', '/', ':' ), '', site_url()));
-$data['posts'] = $count_posts->publish;
-$data['pages'] = $count_pages->publish;
-$data['comments'] = $comments_count->total_comments;
-$data['approved'] = $comments_count->approved;
-$data['spam'] = $comments_count->spam;
-$data['theme_version'] = $theme_data['Version'];
-$data['theme_name'] = $theme_data['Name'];
-$data['site_name'] = str_replace( ' ', '', get_bloginfo( 'name' ));
-$data['plugins'] = $plugin_count;
-$data['plugin'] = urlencode($plugin_name);
-$data['wpversion'] = get_bloginfo('version');
-foreach ( $data as $k => $v ) {
-$url .= $k . '/' . $v . '/';
-}
-$response = wp_remote_get( $url );
-set_transient('presstrends_data', $data, 60*60*24);
-}}
-add_action('admin_init', 'response_presstrends');
-
 /**
 * End
 */
