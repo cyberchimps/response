e861d67065c77df4944bbe146717f2a96edd2522
mfssurya <suryap@mindfiresolutions.com>
2012-07-20 20:55:50 +0530

    - Implemented apple-touch-icon - Fixed issue with image size of uploaded image preview

--- core/classy-options/classy-options-framework/classy-options.php
+++ core/classy-options/classy-options-framework/classy-options.php
@@ -395,7 +395,7 @@ class ClassyOptions {
 			case "upload":
 				// $output .= optionsframework_medialibrary_uploader( $value['id'], $val, null ); // New AJAX Uploader using Media Library	
 				if(isset($val['url'])) {
-					$output .= "Preview: " . "<img src='{$val['url']}' width='350px' height='50px'/><br/>";
+					$output .= "Preview: <br/>" . "<img src='{$val['url']}' class='upload'/><br/>";
 				}
 				$output .= " &nbsp;&nbsp;&nbsp;&nbsp; URL <input type='text' name='{$value['id']}_text' size='72' value='" . (isset($val['url']) ? $val['url'] : "") . "'/>";
 				$output .= " or upload File: <input type='file' id='{$value['id']}' name='{$value['id']}'>";
