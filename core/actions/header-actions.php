e565d1577d4cc323c697cc66003383767d45879a
hugosaner <hugo@sanerdesign.com>
2012-08-15 12:10:58 +0100

    made meta tag language dynamic
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/header-actions.php
+++ core/actions/header-actions.php
@@ -82,8 +82,7 @@ function response_meta_tags() { ?>
 	if(!$post) return; // in case of 404 page or something?>
 
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="distribution" content="global" />
-<meta name="language" content="en" /> 
+<meta name="language" content="<?php bloginfo( 'language' ); ?>" /> 
 <meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; width=device-width"/><?php
 }
 
