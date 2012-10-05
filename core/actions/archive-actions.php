8529e1199ce17015c4e14047321107b2292cb86c
hugosaner <hugo@sanerdesign.com>
2012-10-05 15:24:14 +0100

    Archive actions - replaced printf( __() ) with just _e()
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/archive-actions.php
+++ core/actions/archive-actions.php
@@ -29,25 +29,25 @@ function response_archive_page_title() {
 	global $post; ?>
 	
 		<?php if (is_category()) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Archive for the &#8216;', 'response' )); ?><?php single_cat_title(); ?><?php printf( __( '&#8217; Category:', 'response' )); ?></h2><br />
+			<h2 class="archivetitle"><?php _e( 'Archive for the &#8216;', 'response' ); ?><?php single_cat_title(); ?><?php _e( '&#8217; Category:', 'response' ); ?></h2><br />
 
 		<?php } elseif( is_tag() ) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Posts Tagged &#8216;', 'response' )); ?><?php single_tag_title(); ?><?php printf( __( '&#8217;:', 'response' )); ?></h2><br />
+			<h2 class="archivetitle"><?php _e( 'Posts Tagged &#8216;', 'response' ); ?><?php single_tag_title(); ?><?php _e( '&#8217;:', 'response' ); ?></h2><br />
 
 		<?php } elseif (is_day()) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Archive for', 'response' )); ?> <?php the_time('F jS, Y'); ?>:</h2><br />
+			<h2 class="archivetitle"><?php _e( 'Archive for', 'response' ); ?> <?php the_time('F jS, Y'); ?>:</h2><br />
 
 		<?php } elseif (is_month()) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Archive for', 'response' )); ?> <?php the_time('F, Y'); ?>:</h2><br />
+			<h2 class="archivetitle"><?php _e( 'Archive for', 'response' ); ?> <?php the_time('F, Y'); ?>:</h2><br />
 
 		<?php } elseif (is_year()) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Archive for:', 'response' )); ?> <?php the_time('Y'); ?>:</h2><br />
+			<h2 class="archivetitle"><?php _e( 'Archive for:', 'response' ); ?> <?php the_time('Y'); ?>:</h2><br />
 
 		<?php } elseif (is_author()) { ?>
-			<h2 class="archivetitle"><?php printf( __( 'Author Archive:', 'response' )); ?></h2><br />
+			<h2 class="archivetitle"><?php _e( 'Author Archive:', 'response' ); ?></h2><br />
 
 		<?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<h2 class="archivetitle"><?php printf( __('Blog Archives:', 'response' )); ?></h2><br />
+			<h2 class="archivetitle"><?php _e('Blog Archives:', 'response' ); ?></h2><br />
 	
 		<?php } 
 }
