0ec557bfb2185325d6feba8b206589c4ec2405a8
hugosaner <hugo@sanerdesign.com>
2012-11-14 15:15:33 +0000

    Feed Title - removed repetition
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/core-functions.php
+++ core/core-functions.php
@@ -308,6 +308,9 @@ function response_title_tag_filter( $old_title ) {
 	if (function_exists('is_tag') && is_tag()) { /*Title for tags */
 		$title_tag = get_bloginfo('name').' - Tag Archive for &quot;'.single_tag_title("", FALSE).'&quot;  ';
 	}
+	elseif( is_feed() ) {
+		$title_tag = '';
+	}
 	elseif (is_archive()) { /*Title for archives */ 
 		$title_tag = get_bloginfo('name').$old_title.' Archive '; 
 	}    
